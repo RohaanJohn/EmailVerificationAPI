13a01e26ecedc282db98f835d1f2463b8f3b0ae4
Rohaan John <rohaanrenujohn@gmail.com>
2022-01-23 19:44:30 +0530

    Initial Commit

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 import smtplib
+
+
+import numpy as np
+import cv2
+from IPython.display import display, Javascript
+from base64 import b64decode
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+import webbrowser
 # Create your views here.
 
 def login(request):
@@ -17,7 +27,7 @@ def login(request):
 
     else:
         return render(request,'login.html')
-        return redirect('login')
+        
         
 
 def register(request):
@@ -83,7 +93,58 @@ def contact(request):
         return redirect('/')
   else:
         return render(request,'contact.html')
-        return redirect('contact')
+        
+def trashdetection(request):
+                
+                    
+                    
+                
+                    # Disable scientific notation for clarity
+                    np.set_printoptions(suppress=True)
+
+                    # Load the model
+                    model = tensorflow.keras.models.load_model('C:\\Users\\acer\\Downloads\\keras_model (1).h5')
+
+                    # Create the array of the right shape to feed into the keras model
+                    # The 'length' or number of images you can put into the array is
+                    # determined by the first position in the shape tuple, in this case 1.
+                    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+                    # Replace this with the path to your image
+                    #image = Image.open('C:\\Users\\acer\\Pictures\\Saved Pictures\\person 2.jpg')
+                    image = Image.open()
+
+                    #resize the image to a 224x224 with the same strategy as in TM2:
+                    #resizing the image to be at least 224x224 and then cropping from the center
+                    size = (224, 224)
+                    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+                    #turn the image into a numpy array
+                    image_array = np.asarray(image)
+
+                    
+
+                    # Normalize the image
+                    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+                    # Load the image into the array
+                    data[0] = normalized_image_array
+
+                # run the inference
+                    prediction = model.predict(data)
+                    print(prediction)
+
+                    # condition checking
+                    if prediction[0][1] > prediction[0][0]:
+                    
+                      
+                      return redirect('/')
+                    else:
+                       webbrowser.open('https://akb-alphadebuggers-maps.glitch.me/')
+                       return redirect('/')
+                
+    
+    
  
 
 
