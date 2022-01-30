71d58ed739b8b72a434eead98f3d6cba609578df
Rohaan John <rohaanrenujohn@gmail.com>
2022-01-30 10:28:08 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 import smtplib
-import pandas as pd
+
 
 
 import numpy as np
