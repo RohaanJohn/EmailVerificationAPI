8bda3a244656509bf221ffda2828d723075b1358
Rohaan John <rohaanrenujohn@gmail.com>
2022-01-30 13:13:39 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -71,7 +71,7 @@ def contact(request):
 
         s = smtplib.SMTP('smtp.gmail.com', 587) 
         s.starttls() 
-        s.login("thealphadebuggers@gmail.com", "alphadebuggers12345689")
+        s.login("thealphadebuggers@gmail.com", "alphadebuggers123456789")
         SUBJECT = "Travello"
         TEXT = f"Hi {username}! We will look into your message and send you a reply as soon as possible if needed. Thank you for using Travello!"
         message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
