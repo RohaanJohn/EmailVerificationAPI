0b92528af9aa66cd8ab7c5f338876f158d92cda3
Rohaan John <rohaanrenujohn@gmail.com>
2022-06-28 18:56:57 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -102,7 +102,7 @@ def emotionanalysis(request):
                     np.set_printoptions(suppress=True)
 
                     # Load the model
-                    model = tensorflow.keras.models.load_model('keras_model3.h5')
+                    model = tensorflow.keras.models.load_model('model_unquant.tflite')
 
                     # Create the array of the right shape to feed into the keras model
                     # The 'length' or number of images you can put into the array is
