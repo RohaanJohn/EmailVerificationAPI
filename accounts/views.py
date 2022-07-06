5f5e9d5b1380d717fe05bc43f84598ff55564061
Rohaan John <rohaanrenujohn@gmail.com>
2022-07-06 22:43:45 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -92,8 +92,7 @@ def contact(request):
   else:
         return render(request,'contact.html')
 @api_view(['POST'])
-@csrf_exempt      
-def emotionanalysis(request):
+ def emotionanalysis(request):
                 
               if request.method== 'POST':
                     d = {}
