7e81334909f1da371aeca82b7b5b73830f164a78
Rohaan John <rohaanrenujohn@gmail.com>
2022-07-07 09:07:50 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("register", views.register, name="register"), path("login", views.login, name="login"), path("logout", views.logout, name="logout"),  path("emotionanalysis", views.emotionanalysis, name="emotionanalysis"), path("contact", views.contact, name="contact"), path("emotion", views.emotion, name="emotion")]    
+urlpatterns = [path("register", views.register, name="register"), path("login", views.login, name="login"), path("logout", views.logout, name="logout"),  path("emotionanalysis", views.emotionAnalysis, name="emotionanalysis"), path("contact", views.contact, name="contact"), path("emotion", views.emotion, name="emotion")]    
