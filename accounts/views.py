6ea877d7b7c5b1e7052d235a3f7b6ee3f092ce62
Rohaan John <rohaanrenujohn@gmail.com>
2022-10-31 13:20:57 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,8 @@ import webbrowser
 from rest_framework.decorators import api_view
 from django.core import exceptions, validators
 from rest_framework.response import Response
+import requests
+import json
 # Create your views here.
 
 def login(request):
@@ -98,51 +100,15 @@ def contact(request):
 def emotionAnalysis(request):
                 
               if request.method== 'POST':
-                    #d = {}
-                   
-                    img = request.FILES['img']
-                    # Disable scientific notation for clarity
-                    np.set_printoptions(suppress=True)
-
-                    # Load the model
-                    model = tensorflow.keras.models.load_model('keras_model2.h5')
-
-                    # Create the array of the right shape to feed into the keras model
-                    # The 'length' or number of images you can put into the array is
-                    # determined by the first position in the shape tuple, in this case 1.
-                    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
-                    # Replace this with the path to your image
-                    # image = Image.open('trash1.jpg')
-                    image = Image.open(img)
-
-                    #resize the image to a 224x224 with the same strategy as in TM2:
-                    #resizing the image to be at least 224x224 and then cropping from the center
-                    size = (224, 224)
-                    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-                    #turn the image into a numpy array
-                    image_array = np.asarray(image)
-
-                    # Normalize the image
-                    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-                    # Load the image into the array
-                    data[0] = normalized_image_array
-
-                    # run the inference
-                    prediction = model.predict(data)
-                    #print(prediction)
-
-                    # condition checking
-                    if ((prediction[0][3] > prediction[0][0]) & (prediction[0][3] > prediction[0][1]) & (prediction[0][3] > prediction[0][2])):
-                      return Response({"output":"Fear"})
-                    elif ((prediction[0][2] > prediction[0][0]) & (prediction[0][2] > prediction[0][1]) & (prediction[0][2] > prediction[0][3])):
-                      return Response({"output":"Sad"})
-                    elif ((prediction[0][1] > prediction[0][0]) & (prediction[0][1] > prediction[0][2]) & (prediction[0][1] > prediction[0][3])):
-                      return Response({"output":"Angry"})
-                    else: #that is if angry
-                      return Response({"output":"Happy"})
+                     pic_url = request.FILES['str1']
+                     url = 'http://1193-35-204-252-196.ngrok.io/predict'
+                     #'/content/drive/MyDrive/inputpics/randompic.jpg'
+                     input_data_for_model = {
+                      'str1' : pic_url
+                     }
+                     input_json  = json.dumps(input_data_for_model)
+                     result = requests.post(url, data=input_json)
+                     print(result.text)
 
               else:
                 return render(request,'emotionanalysis.html')
