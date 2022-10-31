2ce857999f263e5bd34a007da7fed61e37b5476d
Rohaan John <rohaanrenujohn@gmail.com>
2022-11-01 00:25:10 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-import smtplib
-import numpy as np
-from PIL import Image, ImageOps
-import webbrowser
 from rest_framework.decorators import api_view
 from django.core import exceptions, validators
 from rest_framework.response import Response
+import smtplib
 import requests
+import json
+
 # Create your views here.
 
 def login(request):
@@ -94,38 +93,28 @@ def contact(request):
         return render(request,'contact.html')
 
 
-@app.post('/predict')
-def predict(input_parameters : model_input):
- test = []
- #real_image = Image.open('/content/drive/MyDrive/Fear.jpg') #store image in temp var to be accessed as such to store in folder
- #image = cv2.imread('/content/drive/MyDrive/Fear.jpg')
- #files = sys.argv[1]
- input_data = input_parameters.json()
- input_dictionary = json.loads(input_data)
- the_url = input_dictionary['str1']
- real_image = Image.open(the_url)
- image = cv2.imread(the_url)
- image = cv2.resize(image, (150,150))
- test.append(image)
- test = np.array(test, dtype='float32')
- test = np.expand_dims(test, axis=1)
- test_image = test[0]
- model = tf.keras.models.load_model('/content/drive/MyDrive/Datasets/keras_model.h5')
- prediction = model.predict(test_image)
- if ((prediction[0][3] > prediction[0][0]) & (prediction[0][3] > prediction[0][1]) & (prediction[0][3] > prediction[0][2])):
-        real_image.save(f'/content/drive/MyDrive/Datasets/Train/Sad/Sad{str(datetime.now())}.jpg', 'JPEG')
-        return('Sad')
- elif ((prediction[0][2] > prediction[0][0]) & (prediction[0][2] > prediction[0][1]) & (prediction[0][2] > prediction[0][3])):
-        real_image.save(f'/content/drive/MyDrive/Datasets/Train/Happy/Happy{str(datetime.now())}.jpg', 'JPEG')
-        return('Happy')
- elif ((prediction[0][1] > prediction[0][0]) & (prediction[0][1] > prediction[0][2]) & (prediction[0][1] > prediction[0][3])):
-        real_image.save(f'/content/drive/MyDrive/Datasets/Train/Fear/Fear{str(datetime.now())}.jpg', 'JPEG')
-        return('Fear')
- elif ((prediction[0][0] > prediction[0][1]) & (prediction[0][0] > prediction[0][2]) & (prediction[0][0] > prediction[0][3])):
-      real_image.save(f'/content/drive/MyDrive/Datasets/Train/Angry/Angry{str(datetime.now())}.jpg', 'JPEG')
-      return('Angry')
- else:
-   return('Null')
+@api_view(['GET', 'POST'])
+def predict(request):
+                
+              if request.method== 'POST':
+                     #pic_url = request.FILES['str1']
+                     pic_url = '/content/drive/MyDrive/Fear.jpg'
+                     #pic_url = '/content/drive/MyDrive/inputpics/randompic.jpg'
+                     url = 'http://0cff-34-86-209-236.ngrok.io/predict'
+                     input_data_for_model = {
+                      'str1' : pic_url
+                     }
+                     input_json  = json.dumps(input_data_for_model)
+                     result = requests.post(url, data=input_json)
+                     the_output = result.text
+                     the_result = the_output.replace('"','')
+                     return Response({"output":the_result})
+                       
+              else:
+                return render(request,'emotion.html')
+   # [(0 is Happy), (1 is Angry), (2 is Sad), (3 is Fear)]
+
+
                       
                 
     
