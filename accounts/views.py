b79b89e9d367b0570004f1fedac7011855bd58b7
Rohaan John <rohaanrenujohn@gmail.com>
2022-11-01 09:34:16 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -120,7 +120,7 @@ def predict(request):
                     string = base64.b64encode(image_bytes)
                     pic_url = f'FinalImage/Image{str(datetime.now())}.jpg'
                     repo.create_file(pic_url, "commit", base64.b64decode(string))
-                    url = 'http://869b-34-138-189-235.ngrok.io/predict'
+                    url = 'http://9fcc-34-138-189-235.ngrok.io/predict'
                     input_data_for_model = {
                       'str1' : pic_url
                     }
